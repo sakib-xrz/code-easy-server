677c396340073a02b712356e3ac8e844530f422c
sakib-xrz <sakibxrz21@gmail.com>
2022-10-24 21:32:44 +0600

    cors install

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = process.env.port || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Code Easy Server Is Running!!!");
