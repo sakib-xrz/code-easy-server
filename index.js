4283e85da8d7ec24107fdbe2bddd08ea97d5b40d
sakib-xrz <sakibxrz21@gmail.com>
2022-10-24 23:10:34 +0600

    course data complete

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require("express");
-var cors = require("cors");
+const express = require('express');
 const app = express();
-const port = process.env.port || 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/course.json")
+
 app.get("/", (req, res) => {
   res.send("Code Easy Server Is Running!!!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
