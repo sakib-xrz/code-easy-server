f0e4e9d868879cd3108454869b5132be8053e35e
sakib-xrz <sakibxrz21@gmail.com>
2022-10-25 23:29:39 +0600

    Course data created

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = courses.find((course) => course.id === id);
+  res.send(singleCourse);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
